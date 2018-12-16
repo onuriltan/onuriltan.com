fc07d6b418e5b8760b79c4b44f9bae4e4cdf0910
onuriltan <onur.iltan@gmail.com>
2018-12-17 00:26:12 +0300

    multilang work wip

---
+++ dist/js/lang.tr.js
@@ -0,0 +1,17 @@
+lang = {
+  home: "Yazılım Mühendisi, Web Programcısı",
+  about:
+    "Merhaba! Ben Onur, yazılım mühendisi ve bilgisayar tutkunu . Okunabilir ve" +
+    "sürdürülebilir bir stilde kod yazmak prensibindeyim, ayrıca son yıllarda olgunlaşmış yeni teknolojiler kullanma taraftarıyım." +
+    "Şu an teknoloji bilgim Java EE 8, Spring Boot, NodeJS, Vue.js, React.js, HTML ve CSS'den oluşmakta.",
+  company1: "Orema Yazılım A.Ş.",
+  company1title: "Full Stack Developer",
+  company1summary:
+    "Şu an Java EE 8 ile Wildfly-Swarm Framework'ü ile backend projelerinde yer almakta, aynı zamanda VueJS, HTML ve SCSS teknolojileri " +
+    "ile de front-end projeleri geliştiriyorum",
+  company2: "Protel Bilgisayar A.Ş.",
+  company2title: "Software Developer",
+  company2summary:
+    "Java Spring Boot, React.js ve SCSS teknolojilerini kullanarak backend ve frontend 'de otel, müşteri yönetimi ve e-fatura yazılımları geliştirmede" +
+    "katkıda bulundum"
+};
