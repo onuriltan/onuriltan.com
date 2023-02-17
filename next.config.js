6ef48aeb77093403fe236875877818a9776a4687
onuriltan <onur.iltan@gmail.com>
2023-02-17 12:39:42 +0300

    feat: add akamai for images

--- next.config.js
+++ next.config.js
@@ -1,22 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = "";
-let basePath = "";
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-  module.exports = {
-    reactStrictMode: true,
-    assetPrefix: assetPrefix,
-    basePath: basePath,
-  };
-} else {
-  module.exports = {
-    reactStrictMode: true,
-  };
-}
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+};
