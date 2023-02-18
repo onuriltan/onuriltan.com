a6b968bf16aa9e1b28a4d21f3c47bc450d2c62c9
onuriltan <onur.iltan@gmail.com>
2023-02-18 12:49:14 +0300

    fix: initial theme context

--- src/store/ThemeContext.tsx
+++ src/store/ThemeContext.tsx
@@ -20,11 +20,13 @@ export function ThemeContextProvider(props: ThemePropsInterface): ReactElement {
     const currentTheme = localStorage.getItem("theme");
     if (r && currentTheme) {
       if (currentTheme === "dark") {
+        setIsDarkTheme(false);
         localStorage.setItem("theme", "light");
         r.style.setProperty("--background-color", "245, 222, 179");
         r.style.setProperty("--foreground-color", "0, 0, 0");
         r.style.setProperty("--foreground-color-secondary", "128, 0, 128");
       } else {
+        setIsDarkTheme(true);
         localStorage.setItem("theme", "dark");
         r.style.setProperty("--background-color", "36, 52, 71");
         r.style.setProperty("--foreground-color", "255, 255, 255");
@@ -38,14 +40,14 @@ export function ThemeContextProvider(props: ThemePropsInterface): ReactElement {
     const currentTheme = localStorage.getItem("theme");
     if (r && currentTheme) {
       if (currentTheme === "dark") {
-        localStorage.setItem("theme", "light");
         setIsDarkTheme(false);
+        localStorage.setItem("theme", "light");
         r.style.setProperty("--background-color", "245, 222, 179");
         r.style.setProperty("--foreground-color", "0, 0, 0");
         r.style.setProperty("--foreground-color-secondary", "128, 0, 128");
       } else {
-        localStorage.setItem("theme", "dark");
         setIsDarkTheme(true);
+        localStorage.setItem("theme", "dark");
         r.style.setProperty("--background-color", "36, 52, 71");
         r.style.setProperty("--foreground-color", "255, 255, 255");
         r.style.setProperty("--foreground-color-secondary", "238, 206, 26");
