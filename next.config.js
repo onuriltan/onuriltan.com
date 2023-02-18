5afecf3717843ba821ae360bda3d989c5a0bed6b
onuriltan <onur.iltan@gmail.com>
2023-02-18 12:24:02 +0300

    fix: app env check

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
+const env = process.env.APP_ENV || false;
 
-if (isGithubActions) {
+if (env === "development") {
+  module.exports = {
+    reactStrictMode: true,
+  };
+} else {
   module.exports = {
     reactStrictMode: true,
     images: {
       loader: "akamai",
       path: "",
     },
-  };
-} else {
-  module.exports = {
-    reactStrictMode: true,
   };
 }
