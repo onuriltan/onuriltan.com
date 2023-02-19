93f712a3a734decb3bef673ebfef62714cf768e4
onuriltan <onur.iltan@gmail.com>
2023-02-19 16:23:51 +0300

    fix: images

--- next.config.js
+++ next.config.js
@@ -1,16 +1,25 @@
 /** @type {import('next').NextConfig} */
 
 const env = process.env.APP_ENV;
-if (env === "development") {
-  module.exports = {
-    reactStrictMode: true,
-  };
-} else {
-  module.exports = {
-    reactStrictMode: true,
-    images: {
-      loader: "akamai",
-      path: "/",
-    },
-  };
-}
+
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    loader: "akamai",
+    path: "/",
+  },
+};
+
+// if (env === "development") {
+//   module.exports = {
+//     reactStrictMode: true,
+//   };
+// } else {
+//   module.exports = {
+//     reactStrictMode: true,
+//     images: {
+//       loader: "akamai",
+//       path: "/",
+//     },
+//   };
+// }
