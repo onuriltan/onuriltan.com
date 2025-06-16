756987e3a7a4b86e9ebf24f34dde4c44c0ed3c38
onuriltan <7993238+onuriltan@users.noreply.github.com>
2025-06-16 11:29:40 +0300

    refactor: dropdown on close remove border after transition

--- src/components/dropdown/index.tsx
+++ src/components/dropdown/index.tsx
@@ -16,6 +16,7 @@ type DropdownProps = {
 const Dropdown = ({ options, selected, onSelect, placeholder }: DropdownProps): JSX.Element => {
   const [open, setOpen] = React.useState(false);
   const ref = React.useRef<HTMLDivElement>(null);
+  const dropdownRef = React.useRef<HTMLDivElement>(null);
 
   React.useEffect(() => {
     function handleClickOutside(event: MouseEvent) {
@@ -28,6 +29,17 @@ const Dropdown = ({ options, selected, onSelect, placeholder }: DropdownProps): 
     return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
 
+  React.useEffect(() => {
+    if (open && dropdownRef.current) {
+      dropdownRef.current.scrollTop = 0; // Reset scroll position when dropdown opens
+    }
+    if (!open && dropdownRef.current) {
+      setTimeout(() => {
+        dropdownRef.current?.classList.remove(styles.dropdown_border);
+      }, 200); // Delay to allow dropdown to close before removing class
+    }
+  }, [open]);
+
   return (
     <div ref={ref} className={styles.container}>
       <div onClick={() => setOpen(!open)} className={styles.wrapper}>
@@ -36,7 +48,10 @@ const Dropdown = ({ options, selected, onSelect, placeholder }: DropdownProps): 
       <div className={`${styles.caret_wrapper} ${open ? styles.caret_wrapper_open : styles.caret_wrapper_closed}`}>
         <i className={`fa-solid fa-caret-down`} />
       </div>
-      <div className={open ? styles.dropdown_open : styles.dropdown_closed}>
+      <div
+        className={`${styles.dropdown_border} ${open ? styles.dropdown_open : styles.dropdown_closed}`}
+        ref={dropdownRef}
+      >
         {options.map((option) => (
           <div
             key={option.value}
