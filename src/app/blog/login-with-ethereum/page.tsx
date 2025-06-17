1fcc737b4f409609d039b8a9350d0ae9bab0648b
onuriltan <7993238+onuriltan@users.noreply.github.com>
2025-06-17 09:35:23 +0300

    feat: for now manually get the blog

--- src/app/blog/login-with-ethereum/page.tsx
+++ src/app/blog/login-with-ethereum/page.tsx
@@ -3,65 +3,59 @@ import { BlogEntry } from "@constants/blogs";
 import AppConstants from "@constants/index";
 import styles from "./index.module.scss";
 
-type Props = {
-  params: {
-    slug: string;
-  };
-};
+type Params = Promise<{ slug: string[] }>;
 
 export function generateStaticParams() {
   const asd = Object.keys(AppConstants.Blogs).map((slug) => ({ slug }));
+  console.log(asd);
   return asd;
 }
 
-export default function BlogPage({ params }: Props) {
-  // TODO: Fix why params comes as promise
+export default async function Page({ params }: { params: Params }) {
+  const theparams = await params;
+  console.log(theparams);
+
   const blog: BlogEntry | undefined = AppConstants.Blogs["login-with-ethereum"];
   if (!blog) notFound();
 
   return (
-    <>
-      <div className={styles.main}>
-        <div className={styles.title_row}>
-          <div />
-          <h1 className={styles.title}>{blog.title}</h1>
-          <p>
-            {String(
-              Intl.DateTimeFormat("en", {
-                year: "numeric",
-                month: "long",
-                day: "2-digit",
-              }).format(new Date(blog.date)),
-            )}
-          </p>
-        </div>
-        <div className={`${styles.content} docs`}>
-          {blog.content.map((content) => {
-            const CustomTag = `${content.tag}`;
-            const CustomTag2 = content.tag2;
-            const parentClassName = content.parentClassName;
-            const subClassName = content.subClassName;
-            if (parentClassName && subClassName && CustomTag2) {
-              return (
-                // @ts-ignore
-                <CustomTag key={JSON.stringify(content)} className={content.parentClassName}>
-                  {/* @ts-ignore */}
-                  <CustomTag2 className={content.subClassName}>{content.text}</CustomTag2>
-                </CustomTag>
-              );
-            }
+    <div className={styles.main}>
+      <div className={styles.title_row}>
+        <div />
+        <h1 className={styles.title}>{blog.title}</h1>
+        <p>
+          {String(
+            Intl.DateTimeFormat("en", {
+              year: "numeric",
+              month: "long",
+              day: "2-digit",
+            }).format(new Date(blog.date)),
+          )}
+        </p>
+      </div>
+      <div className={`${styles.content} docs`}>
+        {blog.content.map((content) => {
+          const CustomTag = `${content.tag}`;
+          const CustomTag2 = content.tag2;
+          const parentClassName = content.parentClassName;
+          const subClassName = content.subClassName;
+          if (parentClassName && subClassName && CustomTag2) {
             return (
               // @ts-ignore
-              <CustomTag
-                key={JSON.stringify(content)}
-                className={content.parentClassName ? content.parentClassName : ""}
-              >
-                {content.text}
+              <CustomTag key={JSON.stringify(content)} className={content.parentClassName}>
+                {/* @ts-ignore */}
+                <CustomTag2 className={content.subClassName}>{content.text}</CustomTag2>
               </CustomTag>
             );
-          })}
-        </div>
+          }
+          return (
+            // @ts-ignore
+            <CustomTag key={JSON.stringify(content)} className={content.parentClassName ? content.parentClassName : ""}>
+              {content.text}
+            </CustomTag>
+          );
+        })}
       </div>
-    </>
+    </div>
   );
 }
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,29 +4,27 @@ import styles from "./index.module.css";
 
 const Blog = () => {
   return (
-    <>
-      <div className={styles.main}>
-        {Constants.BlogList.map((blog) => {
-          return (
-            <Link href={`blog/${blog.id}`} className={styles.row} key={JSON.stringify(blog)}>
-              <div className={styles.title_row}>
-                <h2 className={styles.title}>{blog.title}</h2>
-                <p>
-                  {String(
-                    Intl.DateTimeFormat("en", {
-                      year: "numeric",
-                      month: "long",
-                      day: "2-digit",
-                    }).format(blog.date),
-                  )}
-                </p>
-              </div>
-              <p>{blog.description}</p>
-            </Link>
-          );
-        })}
-      </div>
-    </>
+    <div className={styles.main}>
+      {Constants.BlogList.map((blog) => {
+        return (
+          <Link href={`blog/${blog.id}`} className={styles.row} key={JSON.stringify(blog)}>
+            <div className={styles.title_row}>
+              <h2 className={styles.title}>{blog.title}</h2>
+              <p>
+                {String(
+                  Intl.DateTimeFormat("en", {
+                    year: "numeric",
+                    month: "long",
+                    day: "2-digit",
+                  }).format(blog.date),
+                )}
+              </p>
+            </div>
+            <p>{blog.description}</p>
+          </Link>
+        );
+      })}
+    </div>
   );
 };
 
