4b1e8ac73bf959e7d0eedc9418d24da387d5d77e
onuriltan <7993238+onuriltan@users.noreply.github.com>
2025-06-17 09:45:44 +0300

    fix: next config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,22 @@
 /** @type {import('next').NextConfig} */
-
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
+
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "drive.google.com",
+        pathname: "/**", // added to support all image paths
       },
     ],
+    formats: ["image/webp"], // serve WebP when supported
+  },
+  experimental: {
+    serverActions: true, // if using server actions
+    appDir: true, // App Router enabled
+    mdxRs: true, // if you're using MDX (can be removed otherwise)
   },
 };
+
+module.exports = nextConfig;
